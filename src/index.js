b7849907737c797062d4739348022a617b58a1e9
elprogramador2021 <95893744+elprogramador2021@users.noreply.github.com>
2022-07-09 16:45:05 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,13 +5,12 @@ import App from './containers/App';
 import { StateProvider } from './StateProvider';
 import reducer, { initialState } from './reducer';
 
-//ReactDOM.render(<App />, document.getElementById('app'));
 ReactDOM.render(
-  <React.StrictMode>
+  <>
     <StateProvider initialState={initialState} reducer={reducer}>
       <App />
     </StateProvider>
 
-  </React.StrictMode>,
+  <>,
   document.getElementById('app'),
 );
